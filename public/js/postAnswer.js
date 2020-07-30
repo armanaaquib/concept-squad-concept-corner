1e9328f2a94fa3b30aa0ba171a6fb735c2dc7593
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-07-30 11:30:57 +0530

    |#7|Aaquib/Neha| Mark an answer as correct
    - Updating reactions on front end instedad of reloading of image

--- public/js/postAnswer.js
+++ public/js/postAnswer.js
@@ -2,65 +2,70 @@ const showDescription = (description, id) => {
   const quill = new Quill(`#${id}`, {
     modules: {
       syntax: true,
-      toolbar: false
+      toolbar: false,
     },
     readOnly: true,
-    theme: 'snow'
+    theme: 'snow',
   });
 
   quill.setContents(JSON.parse(description));
 };
 
-const postAnswer = function(editor) {
+const postAnswer = function (editor) {
   const questionId = document.getElementById('h_qId').value;
   fetch('/postAnswer', {
     method: 'POST',
     body: JSON.stringify({
       questionId,
-      answer: JSON.stringify(JSON.stringify(editor.getContents()))
+      answer: JSON.stringify(JSON.stringify(editor.getContents())),
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   }).then(() => {
     window.location = `/question/${questionId}`;
   });
 };
 
-const showPostAnswerEditor = function() {
+const showPostAnswerEditor = function () {
   const postAnswerEditor = new Quill('#editor-postAnswer', {
     modules: {
       syntax: true,
       toolbar: [
         [{ size: ['small', false, 'large', 'huge'] }],
         ['bold', 'italic', 'underline'],
-        ['code-block']
-      ]
+        ['code-block'],
+      ],
     },
     placeholder: 'description...',
-    theme: 'snow'
+    theme: 'snow',
   });
   hljs.configure({
-    languages: ['javascript', 'ruby', 'python']
+    languages: ['javascript', 'ruby', 'python'],
   });
   const postAnswerBtn = document.getElementById('postAnswer-btn');
   postAnswerBtn.onclick = postAnswer.bind(null, postAnswerEditor);
 };
 
-const markAccepted = answer => {
+const markAccepted = (answer) => {
   fetch('/markAccepted', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       questionId: answer.questionId,
       answerId: answer.answerId,
-      username: answer.username
-    })
-  }).then(res => {
+      username: answer.username,
+    }),
+  }).then((res) => {
     if (res.status === 200) {
-      window.location = window.location.href;
+      $('.reactions .unchecked-answer').remove();
+      const checkedAnswer = document.createElement('div');
+      checkedAnswer.classList.add('col-3');
+      checkedAnswer.classList.add('checked-answer');
+      checkedAnswer.innerHTML = '<i class="material-icons">check_circle</i>';
+      $(`.reactions #a-${answer.answerId}`).append(checkedAnswer);
     }
   });
 };
