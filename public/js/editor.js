c5b6ef62544adb96202ead62d9840fb91ae9da12
rajat <rajat4113@gmail.com>
2020-07-30 13:54:27 +0530

    |#3|Rajat/Sruthy|tag a question
    -user can now add multiple tags to a question
    -added style and function to show tags and remove tags

--- public/js/editor.js
+++ public/js/editor.js
@@ -22,6 +22,12 @@ const validateForm = () => {
   return title !== '';
 };
 
+const getSelectedTags = function() {
+  const selectedTagsHTML = document.querySelectorAll('.selected-tag');
+  const selectedTags = Array.from(selectedTagsHTML);
+  return selectedTags.map(selectedTag => selectedTag.innerText);
+};
+
 const postQuestion = () => {
   if (!validateForm()) {
     const errorTitle = getElement('errorTitle');
@@ -30,10 +36,9 @@ const postQuestion = () => {
   }
   const title = getElement('title').value;
   const description = JSON.stringify(JSON.stringify(quill.getContents()));
-  const tags = getElement('tags').value;
   fetch('/postQuestion', {
     method: 'POST',
-    body: JSON.stringify({ title, description, tags: tags.split(' ') }),
+    body: JSON.stringify({ title, description, tags: getSelectedTags() }),
     headers: {
       'Content-Type': 'application/json'
     }
@@ -46,4 +51,50 @@ const postQuestion = () => {
     .then(questionId => {
       window.location = `/question/${questionId}`;
     });
+};
+
+const getCancelButton = function() {
+  return `<svg class="svg-icon iconClearSm pe-none" width="14" 
+            height="14" viewBox="0 0 14 14">
+  <path d="M12 3.41L10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 
+  3.41 12 7 8.41 10.59 12 12 10.59 8.41 7z">
+  </path></svg>`;
+};
+
+const removetag = function(tagToRemove) {
+  tagToRemove.parentElement.remove();
+};
+
+const setTagsFieldWidth = function() {
+  const selectedTagsWidth = document.querySelector('.selected-tags')
+    .offsetWidth;
+  const tagsBoxWidth = document.querySelector('.tagsBox').offsetWidth;
+  const tagsField = getElement('tags');
+  tagsField.style.width = tagsBoxWidth - selectedTagsWidth - 40;
+};
+
+const removeLastTag = function(tagField) {
+  const selectedTagsHTML = document.querySelectorAll('.selected-tag');
+  if (selectedTagsHTML.length == 0) {
+    return;
+  }
+  const selectedTags = Array.from(selectedTagsHTML);
+  const tagToRemove = selectedTags[selectedTags.length - 1];
+  tagField.value = tagToRemove.innerText;
+  tagToRemove.remove();
+};
+
+const createTag = function(tagField) {
+  if (window.event.keyCode == 8 && tagField.value == '') {
+    removeLastTag(tagField);
+  }
+  if (window.event.keyCode == 32) {
+    const tag = tagField.value;
+    const selectedTags = document.querySelector('.selected-tags');
+    const tagHtml = `<span class="selected-tag">${tag}<a class="removetag"
+     onclick="removetag(this)">${getCancelButton()}</a></span>`;
+    selectedTags.innerHTML += tagHtml;
+    tagField.value = '';
+  }
+  setTagsFieldWidth();
 };
